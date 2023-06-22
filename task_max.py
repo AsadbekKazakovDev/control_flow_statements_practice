8347b7595b1ad15802b9f1ca91b3922931c17a61
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-22 23:51:22 +0500

    yuklandi 2

--- task_max.py
+++ task_max.py
@@ -9,7 +9,14 @@ def find_max(a,b,c):
     return:
         int
     """
-    return 0
+    m = a
+    if m<b:
+        m=b
+    if m<c:
+        m=c
+    return m
+a,b,c = 3, 8,21
+print(find_max(a,b,c))
 
 # Example:
 # find_max(1,2,3) -> 3
@@ -25,7 +32,15 @@ def find_max_idx(a,b,c):
     return:
         int
     """
-    return 0
+    f = 1
+    d = a
+    if d<b:
+        f+=1
+    if d<c:
+        f+=2
+    return f
+a,b,c = 100,2,13
+print(find_max_idx(a,b,c))
 
 # Example:
 # find_max_idx(10,2,13) -> 3
